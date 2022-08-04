33b5436861ef052fb12aa4e2a559347be9f6bca7
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-08-04 20:18:19 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { GlobalStyle } from './styles/global';
+
 function App() {
   return (
     <div className="App">
       <h1>Dt Money</h1>
+
+      <GlobalStyle />
     </div>
   );
 }
