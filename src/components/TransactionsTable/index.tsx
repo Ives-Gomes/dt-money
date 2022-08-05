d12fa567b05582233693a294cf9ffeb56c6e7f26
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-08-05 16:07:08 -0300

    feat: add and configure miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Container } from './styles';
 
-const TransactionsTable: React.FC = () => (
-  <Container>
-    <table>
-      <thead>
-        <tr>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </tr>
-      </thead>
+const TransactionsTable: React.FC = () => {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
 
-      <tbody>
-        <tr>
-          <td>Desenvolvimento de Website</td>
-          <td className="deposit">R$2.000,00</td>
-          <td>Desenvolvimento</td>
-          <td>05/08/2022</td>
-        </tr>
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
 
-        <tr>
-          <td>Aluguel</td>
-          <td className="withdraw">- R$1.000,00</td>
-          <td>Casa</td>
-          <td>15/07/2022</td>
-        </tr>
-      </tbody>
-    </table>
-  </Container>
-);
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$2.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>05/08/2022</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.000,00</td>
+            <td>Casa</td>
+            <td>15/07/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+};
 
 export default TransactionsTable;
