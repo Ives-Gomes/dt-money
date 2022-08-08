239c17e16affeb1e38a2f301ec6897324accae45
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-08-08 15:36:31 -0300

    feat: add new transaction types buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Modal from 'react-modal';
 
 import closeImg from '@assets/close.svg';
+import incomeImg from '@assets/income.svg';
+import outcomeImg from '@assets/outcome.svg';
 
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -30,6 +32,23 @@ const NewTransactionModal = ({ isOpen, onRequestClose }: NewTransactionModalProp
 
       <input placeholder="Título" />
       <input type="number" placeholder="Valor" />
+
+      <TransactionTypeContainer>
+        <button
+          type="button"
+        >
+          <img src={incomeImg} alt="Entrada" />
+          <span>Entrada</span>
+        </button>
+
+        <button
+          type="button"
+        >
+          <img src={outcomeImg} alt="Saída" />
+          <span>Saída</span>
+        </button>
+      </TransactionTypeContainer>
+
       <input placeholder="Caregoria" />
 
       <button type="submit">
